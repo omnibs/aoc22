6adaa1071482dc917edbb2c05a3700a71dcfb397
Juliano Solanho <juliano.solanho@gmail.com>
2022-12-01 11:33:30 -0300

    Use newer nixpkgs with newer cargo (still borked)

--- shell.nix
+++ shell.nix
@@ -2,7 +2,8 @@ let
   sources = import ./nix/sources.nix { };
   pkgs = import sources.nixpkgs { };
   roc = import sources.roc {
-    cargoSha256 = "0000000000000000000000000000000000000000000000000000";
+    cargoSha256 = "sha256-AH/cWRbshJI2pweoz24AXcDcz/+fM6cGHJU7V9GH/w4=";
+    pkgs = pkgs;
   };
 in pkgs.mkShell {
   buildInputs = [
